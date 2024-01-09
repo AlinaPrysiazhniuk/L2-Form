965866f0ae79bb0af4486589251ed5f7e59c5d05
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-01-09 19:26:46 +0200

    form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    name: '',
+    tag: '',
+  };
+
+  // handleNameChange = event => {
+  //   this.setState({ name: event.currentTarget.value });
+  // };
+
+  // handleTagChange = event => {
+  //   this.setState({ tag: event.currentTarget.value });
+  // };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    // console.log(event.currentTarget);
+    // console.log(event.currentTarget.name);
+    // console.log(event.currentTarget.value);
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            ></input>
+          </label>
+          <label>
+            Surname
+            <input
+              type="text"
+              name="tag"
+              value={this.state.tag}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Send</button>
+        </form>
+      </>
+    );
+  }
+}
