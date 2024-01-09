d343baff77457405af3d02d803b3e22df4aac28d
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-01-09 20:23:23 +0200

    radio button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,7 @@ export class Form extends Component {
     //state форми потрібен для зберігання даних, що змінюються під час введення в інпути
     name: '',
     tag: '',
+    experience: 'junior',
   };
 
   handleChange = event => {
@@ -37,6 +38,7 @@ export class Form extends Component {
             onChange={this.handleChange}
           ></input>
         </label>
+        <br />
         <label>
           Surname
           <input
@@ -46,6 +48,41 @@ export class Form extends Component {
             onChange={this.handleChange}
           />
         </label>
+
+        <p>Ваш рівень розрбника: </p>
+
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="junior"
+            onChange={this.handleChange}
+            checked={this.state.experience === 'junior'}
+          />
+          Junior
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="middle"
+            onChange={this.handleChange}
+            checked={this.state.experience === 'middle'}
+          />
+          Middle
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="experience"
+            value="senior"
+            onChange={this.handleChange}
+            checked={this.state.experience === 'senior'}
+          />
+          Senior
+        </label>
+        <br />
         <button type="submit">Send</button>
       </form>
     );
